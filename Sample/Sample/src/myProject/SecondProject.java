0d2ba846e1b7eb3cde919aca8c87a9109cdd87c6
Jaganv1998 <123948253+Jaganv1998@users.noreply.github.com>
2023-03-11 15:15:09 +0530

    First Commit.........

---
+++ Sample/Sample/src/myProject/SecondProject.java
@@ -0,0 +1,13 @@
+package myProject;
+
+public class SecondProject {
+	
+	public static void main(String[] args)
+	
+	{
+		int a = 5;
+		int b =8;
+		System.out.println("Addition of two numbers: " + (a+b));
+	}
+
+}
