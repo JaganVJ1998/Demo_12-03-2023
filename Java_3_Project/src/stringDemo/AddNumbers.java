709c40dff6e9a0d47463df32e97e1e0709d93950
Jaganv1998 <123948253+Jaganv1998@users.noreply.github.com>
2023-03-12 11:25:59 +0530

    Second Commit

---
+++ Java_3_Project/src/stringDemo/AddNumbers.java
@@ -0,0 +1,12 @@
+package stringDemo;
+
+public class AddNumbers {
+
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 9;
+
+		System.out.println("Addition of two numbers: " + (a+b));
+	}
+
+}
