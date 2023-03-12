cfc66ebc15284eff8ed57f65f234fc3ad7e0cea7
Jaganv1998 <123948253+Jaganv1998@users.noreply.github.com>
2023-03-12 11:08:05 +0530

    First Commit

---
+++ Java_3_Project/src/stringDemo/StringProject.java
@@ -0,0 +1,21 @@
+package stringDemo;
+
+public class StringProject {
+	
+	int a =3;
+	int b =4;
+	
+	public void addnum()
+	{
+		
+	}
+	
+	public static void main(String [] args)
+	{
+		
+		String s = "Hello";
+		s.concat("Welcome all") ;
+		System.out.println(s);
+	}
+	
+}
